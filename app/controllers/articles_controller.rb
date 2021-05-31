363a6dd5d8b6cb201e9a9f5060522af46a876c83
Alexander Makarov <a.makarov@molinos.ru>
2021-05-31 11:35:56 +0700

    articles controller refactored

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
   def show
-    @article = Article.find(params[:id])
   end
   def new
     @article = Article.new
   end
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       # render plain: @article.inspect
       flash[:success] = "Article successfully created"
-      # redirect_to article_path(@article)
       redirect_to @article
     else
       flash[:error] = "Something went wrong"
@@ -21,11 +20,9 @@ class ArticlesController < ApplicationController
     end
   end
   def edit
-    @article = Article.find(params[:id])
   end
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to @article
     else
@@ -34,7 +31,6 @@ class ArticlesController < ApplicationController
     end
   end
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = 'Article was successfully deleted.'
       redirect_to articles_url
@@ -42,5 +38,15 @@ class ArticlesController < ApplicationController
       flash[:error] = 'Something went wrong'
       redirect_to articles_url
     end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
