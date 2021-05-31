2cf02a90ec920ce436551fce98895295b7a48943
Alexander Makarov <a.makarov@molinos.ru>
2021-05-31 10:10:00 +0700

    destroy and home

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,4 +33,14 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:success] = 'Article was successfully deleted.'
+      redirect_to articles_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to articles_url
+    end
+  end
 end
