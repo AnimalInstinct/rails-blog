aa981543ab5ee93fd242b19af94d247e626d9306
Alexander Makarov <a.makarov@molinos.ru>
2021-05-31 08:47:20 +0700

    create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,17 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:success] = "Object successfully created"
+      # render plain: @article.inspect
+      redirect_to @article
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
 end
