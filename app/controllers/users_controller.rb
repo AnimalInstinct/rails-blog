9d954695910f63efc8c16992010686df09e33591
Alexander Makarov <a.makarov@molinos.ru>
2021-06-02 12:02:09 +0700

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(article_params)
+    if @user.save
+      flash[:success] = "User successfully created. Welcome #{ @user.username } "
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def article_params
+    params.permit(:username, :email, :password)
+  end
 end
