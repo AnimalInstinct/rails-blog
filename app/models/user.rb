895d1f482a649f355e19c9d422373d487576e79d
Alexander Makarov <a.makarov@molinos.ru>
2021-06-02 10:04:14 +0700

    email to lowercase before save user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_many :articles
-  validates :username, presence: true, 
-                      length: { minimum: 3, maximum: 25 }, 
+  before_save { self.email = email.downcase }
+  validates :username, presence: true,
+                      length: { minimum: 3, maximum: 25 },
                       uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, 
-                    length: { maximum: 105 }, 
+  validates :email, presence: true,
+                    length: { maximum: 105 },
                     uniqueness: true,
                     format: { with: VALID_EMAIL_REGEX }
 end
