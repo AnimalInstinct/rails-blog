c53026d6d99f1377bd8d2f82620fa176e1dc5638
Alexander Makarov <a.makarov@molinos.ru>
2021-06-04 09:34:47 +0700

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def def new
     @user = User.new
   end
 
   def create
-    @user = User.new(article_params)
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "User successfully created. Welcome #{ @user.username } "
       redirect_to @user
@@ -15,10 +16,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
-  def article_params
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "User was successfully updated"
+      redirect_to @user
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def user_params
     params.permit(:username, :email, :password)
   end
 end
