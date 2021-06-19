621fffba2078c3ac3a56fbd78f0690681fb27a3c
Alexander <alexhelloworld@protonmail.com>
2021-06-19 14:27:19 +0700

    controllers test refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
   root 'pages#index'
   get 'about', to: 'pages#about'
-  resources :articles
   get 'signup', to: 'users#new'
-  resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :users, except: [:new]
+  resources :categories
+  resources :articles
 end
