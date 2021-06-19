246b9c563daf9ec6d457b667b2178add3ec48b5e
Alexander <alexhelloworld@protonmail.com>
2021-06-19 13:05:01 +0700

    category model test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,50 +1,50 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy ]
+  # before_action :set_category, only: [:show, :edit, :update, :destroy ]
 
-  def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)
-  end
+  # def index
+  #   @categories = Category.paginate(page: params[:page], per_page: 5)
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def new
-    @category = Category.new
-  end
+  # def new
+  #   @category = Category.new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      flash[:info] = "Category was successfully created"
-      redirect_to @category
-    else
-      flash[:info] = "Something went wrong"
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @category = Category.new(category_params)
+  #   if @category.save
+  #     flash[:info] = "Category was successfully created"
+  #     redirect_to @category
+  #   else
+  #     flash[:info] = "Something went wrong"
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def update
-    if @category.update(category_params)
-      flash[:info] = "Category was successfully updated"
-      redirect_to @category
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @category.update(category_params)
+  #     flash[:info] = "Category was successfully updated"
+  #     redirect_to @category
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
-  def destroy
-    @category.destroy
-  end
+  # def destroy
+  #   @category.destroy
+  # end
 
-  private
-  def set_category
-    @category = Category.find(params[:id])
-  end
+  # private
+  # def set_category
+  #   @category = Category.find(params[:id])
+  # end
 
-  def category_params
-    params.require(:category).permit(:name, :description)
-  end
+  # def category_params
+  #   params.require(:category).permit(:name, :description)
+  # end
 end
