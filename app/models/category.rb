df3a2e3cf62c30bed55b41a06d06ade5276157c1
Alexander <alexhelloworld@protonmail.com>
2021-07-15 12:37:34 +0700

    Categories and Articles ApplicationRecord associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates_uniqueness_of :name
-  # validates :description, presence: true, length: { minimum: 10 }
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
